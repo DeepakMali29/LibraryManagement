39045d8a787312420d5a8a811f9e1f032f96be74
Deepak Mali <deepak.mali@civica2.com>
2022-08-08 19:22:41 +0530

    Add project files.

---
+++ LibraryManagement/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryManagement.Controllers
+{
+    public class AccountController : Controller
+    {
+        // Send an OpenIDConnect Sign-In Request
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                try
+                {
+                HttpContext.GetOwinContext().Authentication.Challenge(new AuthenticationProperties { RedirectUri = "/" },
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType);
+                }
+                catch (Exception ex) 
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        // Signs the user out and clears the cache of access tokens.  
+        public void SignOut()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut(
+                OpenIdConnectAuthenticationDefaults.AuthenticationType, CookieAuthenticationDefaults.AuthenticationType);
+        }
+    }
+}
---
+++ LibraryManagement/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System.Configuration;
+using System.Globalization;
+using System.Threading.Tasks;
+using Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.Owin.Host.SystemWeb;
+using Microsoft.Owin.Security.Notifications;
+using Microsoft.IdentityModel.Protocols;
+using System;
+
+namespace LibraryManagement
+{
+    public partial class Startup
+    {
+
+        // Get key values from web.config file
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string tenant = ConfigurationManager.AppSettings["ida:Tenant"];
+        private static string addInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+        private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
+
+        // Concatenate aadInstance, tenant to form authority value 
+        private string authority = string.Format(CultureInfo.InvariantCulture + addInstance + tenant);
+
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+        private void ConfigureAuth(IAppBuilder app)
+        {
+            try
+            {
+
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                //CookieManager = new SystemWebCookieManager()
+                AuthenticationType = "Cookies",
+                ExpireTimeSpan = TimeSpan.FromMinutes(60), // Expire after an hour
+                SlidingExpiration = true // use sliding expiration..
+            });
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = clientId,
+                    Authority = authority,
+                    PostLogoutRedirectUri = postLogoutRedirectUri,
+                    Notifications = new OpenIdConnectAuthenticationNotifications
+                    {
+                        AuthenticationFailed = (context) =>
+                        {
+                            context.HandleResponse();
+                            context.OwinContext.Response.Redirect("/Home/Index");
+                            return Task.FromResult(0);
+                        }
+                    },
+                    UseTokenLifetime = false
+                });
+            }
+            catch (TaskCanceledException ex)
+            {
+                    
+            }
+        }
+    }
+}
